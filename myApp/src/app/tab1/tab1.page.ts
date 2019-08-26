ab247dbbd3f68e3e940303ebaeb2d540cb6e2c64
宋博 <songbo@songbodeMacBook-Air.local>
2019-08-26 23:58:19 +0800

    添加服务器镜头列表请求

--- myApp/src/app/tab1/tab1.page.ts
+++ myApp/src/app/tab1/tab1.page.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
-import {NavController, AlertController} from "@ionic/angular";
+import { Component, ViewChild } from '@angular/core';
+import {NavController, AlertController, IonInfiniteScroll} from "@ionic/angular";
 
 import { ActivatedRoute } from '@angular/router';
+
+import { HTTP } from "@ionic-native/http/ngx"
 
 
 @Component({
@@ -10,10 +12,15 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  // @ts-ignore
+  @ViewChild(IonInfiniteScroll) infiniteScroll: IonInfiniteScroll;
   public username_about:String;
+  public datas;
+
   constructor(public navCtrl: NavController,
               public alertCtrl: AlertController,
-              public route:ActivatedRoute
+              public route:ActivatedRoute,
+              public httpCtrl: HTTP
               ) {}
 
   ngOnInit() {
@@ -23,7 +30,35 @@ export class Tab1Page {
       this.username_about = data.username;
     })
 
+    //请求服务器镜头列表接口
+    var that = this;
+    this.httpCtrl.get('http://scenemanger.zeroboy.cn/api.php/Camera/getlist',{},{})
+        .then((data) => {
+
+          let res = JSON.parse(data.data);
+          that.datas = res['data'];
+          console.log(111111);
+          console.log(that.datas);
+        })
+
   }
+
+  // loadData(event) {
+  //   setTimeout(() => {
+  //     console.log('Done');
+  //     event.target.complete();
+  //
+  //     // App logic to determine if all data is loaded
+  //     // and disable the infinite scroll
+  //     if (this.datas.length == 1000) {
+  //       event.target.disabled = true;
+  //     }
+  //   }, 500);
+  // }
+  //
+  // toggleInfiniteScroll() {
+  //   this.infiniteScroll.disabled = !this.infiniteScroll.disabled;
+  // }
 
 
 }

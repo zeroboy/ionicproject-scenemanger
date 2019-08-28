f3cfc8d5dce946b6859ca6d365da84108247d797
Your Name <2586073409@qq.com>
2019-08-28 18:29:06 +0800

    '服务器列表完成'

--- myApp/src/app/tab1/tab1.page.ts
+++ myApp/src/app/tab1/tab1.page.ts
@@ -7,87 +7,64 @@ import { HTTP, } from "@ionic-native/http/ngx"
 
 import {NgxDatatableModule} from '@swimlane/ngx-datatable'
 
+import { Observable } from 'rxjs';
+
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: [
+      'tab1.page.scss',
+      '../../assets/css/theme/bootstrap.scss',
+      '../../assets/css/theme/dark.scss'
+  ]
 })
 export class Tab1Page {
-  // @ts-ignore
-  @ViewChild(IonInfiniteScroll) infiniteScroll: IonInfiniteScroll;
-    public username_about:String;
     // @ts-ignore
-    public cameralist = [];
-    // @ts-ignore
-    public columns = [];
+    @ViewChild(IonInfiniteScroll) infiniteScroll: IonInfiniteScroll;
+    rows: Observable<any[]>;
+    public subscriberObj;
+    public pagination: any = {
+        "totalResults": 0,
+        "currentPage": 1,
+        "pageSize": 10
+    };
+    public cameralist: any = [];
 
-  constructor(public navCtrl: NavController,
-              public alertCtrl: AlertController,
-              public route:ActivatedRoute,
-              public httpCtrl: HTTP,
-              public ngxDataTable: NgxDatatableModule
-              ) {
-      //this.cameralist = [{"id":"4","name":"封神榜","place_num":"160011","lens_type":"36","fodder_num":"A058C049","take_message":"无","add_time":"2019-08-24 21:10:48","lens_make_message":"下方岩壁延伸，中..."},{"id":"5","name":"封神榜","place_num":"160011","lens_type":"36","fodder_num":"A058C049","take_message":"无","add_time":"2019-08-24 21:10:48","lens_make_message":"下方岩壁延伸，中..."},{"id":"6","name":"封神榜","place_num":"160011","lens_type":"36","fodder_num":"A058C049","take_message":"无","add_time":"2019-08-24 21:10:48","lens_make_message":"下方岩壁延伸，中..."},{"id":"7","name":"封神榜","place_num":"160011","lens_type":"36","fodder_num":"A058C049","take_message":"无","add_time":"2019-08-24 21:10:48","lens_make_message":"下方岩壁延伸，中..."},{"id":"8","name":"封神榜","place_num":"160011","lens_type":"36","fodder_num":"A058C049","take_message":"无","add_time":"2019-08-24 21:10:48","lens_make_message":"下方岩壁延伸，中..."},{"id":"9","name":"封神榜","place_num":"160011","lens_type":"43","fodder_num":"123456","take_message":"无","add_time":"2019-08-24 21:10:48","lens_make_message":"下方岩壁延伸，中..."},{"id":"10","name":"封神榜","place_num":"160011","lens_type":"36","fodder_num":"A058C049","take_message":"无","add_time":"2019-08-24 21:10:48","lens_make_message":"下方岩壁延伸，中..."},{"id":"11","name":"封神榜","place_num":"160011","lens_type":"36","fodder_num":"A058C049","take_message":"无","add_time":"2019-08-24 21:10:48","lens_make_message":"下方岩壁延伸，中..."},{"id":"12","name":"封神榜","place_num":"160011","lens_type":"36","fodder_num":"A058C049","take_message":"无","add_time":"2019-08-24 21:10:48","lens_make_message":"下方岩壁延伸，中..."},{"id":"13","name":"封神榜","place_num":"160011","lens_type":"36","fodder_num":"A058C049","take_message":"无","add_time":"2019-08-24 21:10:48","lens_make_message":"下方岩壁延伸，中..."}];
-      /*this.fetch((data) => {
-          this.cameralist = data;
-      });*/
-      this.getlist(1);
-  }
+    columns = [
+        { prop: 'id', name: 'ID', frozenLeft: true , width:50},
+        { prop: 'name', name: '项目名', width:50},
+        { prop: 'place_num', name: '场号' ,width:50},
+        { prop: 'fodder_num', name: '镜头号' ,width:50},
+        { prop: 'lens_type', name: '素材号', width:50 },
+        { prop: 'take_message', name: 'take信息', width:80},
+        { prop: 'lens_make_message', name: '制作镜头信息', width:120},
+    ];
 
+    constructor(public navCtrl: NavController,
+                public alertCtrl: AlertController,
+                public route:ActivatedRoute,
+                public httpCtrl: HTTP,
+                public ngxDataTable: NgxDatatableModule
+    ) {
+        this.rows = Observable.create((subscriber) => {
+            this.getlist(1,(data) => {
+                console.log('start....');
+                console.log(data);
+                this.cameralist = data;
+                subscriber.next(this.cameralist);
+                //subscriber.next(data.splice(15, 30));
+                //subscriber.complete();
+                this.subscriberObj = subscriber;
 
-  ngOnInit() {
-    //获取路由参数
-    this.route.queryParams.subscribe((data) => {
-      console.log(data);
-      this.username_about = data.username;
-    })
+            });
+        });
 
-   //
-      this.columns = [
-          { prop: 'id', name: 'ID', frozenLeft: true },
-          { prop: 'name', name: '项目名'},
-          { prop: 'place_num', name: '场号' },
-          { prop: 'fodder_num', name: '镜头号' },
-          { prop: 'lens_type', name: '素材号' },
-          { prop: 'take_message', name: 'take信息' },
-          { prop: 'lens_make_message', name: '制作镜头信息' },
-      ];
-
-      //this.getlist(1);
-
-  }
-    ionViewWillEnter() {
-
+        // Rx.DOM.ajax({ url: '/products', responseType: 'json'}).subscribe()
+        // this.rows = Observable.from(rows);
     }
 
-  //请求服务器镜头列表接口
-  getlist(curr) {
-      const options = {
-          method: "post",
-          data: { curr: curr}
-      };
-      var that = this;
-      //var all =  this.cameralist;
-      // @ts-ignore
-      this.httpCtrl.sendRequest('http://scenemanger.zeroboy.cn/api.php/Camera/getlist',options)
-          .then((data) => {
-
-              let res = JSON.parse(data.data);
-              for (var i=0;i<res['data'].length;i++){
-                  that.cameralist.push(res['data'][i]);
-              }
-          })
-          .catch((err) => {
-
-          });
-
-      //that.cameralist = all;
-      console.log(222222333);
-      console.log(that.cameralist);
-  }
-
-    fetch(cb) {
+    /*fetch(cb) {
         const req = new XMLHttpRequest();
         req.open('GET', `http://scenemanger.zeroboy.cn/api.php/Camera/getlist`);
 
@@ -99,31 +76,63 @@ export class Tab1Page {
         };
 
         req.send();
+    }*/
+
+    //请求服务器镜头列表接口
+    getlist(curr,cb) {
+        const options = {
+            method: "post",
+            data: { curr: curr}
+        };
+        var that = this;
+        //var all =  this.cameralist;
+        // @ts-ignore
+        this.httpCtrl.sendRequest('http://scenemanger.zeroboy.cn/api.php/Camera/getlist',options)
+            .then((data) => {
+
+                let res = JSON.parse(data.data);
+                let all = res['data'];
+                this.pagination.totalResults += res['data'].length;
+                this.pagination.currentPage = parseInt(String(this.pagination.totalResults / this.pagination.pageSize));
+                cb(all);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+
     }
 
-  loadData(event) {
-    setTimeout(() => {
-      console.log('Done');
-      event.target.complete();
+    loadData(event) {
+        setTimeout(() => {
+          console.log('Done');
+          event.target.complete();
 
-      var nowlwn = this.cameralist.length;
-      console.log('len:'+String(nowlwn));
-      var curr = (nowlwn == 0)?1:parseInt(String(nowlwn / 10));
+            // App logic to determine if all data is loaded
+            // and disable the infinite scroll
+                if (this.pagination.totalResults % this.pagination.pageSize != 0) {
+                    event.target.disabled = true;
+                    return;
+                }
+                let curr = (this.pagination.totalResults == 0)?1:(this.pagination.currentPage+1);
+                //let subscriberObj = this.subscriberObj;
+                this.getlist(curr ,(data) => {
+                    console.log('loadData....');
+                    console.log(data);
+                    this.cameralist = this.cameralist.concat(data);
+                    console.log('cameralist');
+                    console.log(this.cameralist);
+                    this.subscriberObj.next(this.cameralist);
+                    console.log(this.subscriberObj);
+                });
+        }, 2000);
+    }
 
-        console.log('curr:'+String(curr));
-      // App logic to determine if all data is loaded
-      // and disable the infinite scroll
-      if (nowlwn % 10 != 0) {
-        event.target.disabled = true;
-      }else{
-          this.getlist(curr);
-      }
-    }, 2000);
-  }
+    toggleInfiniteScroll() {
+        this.infiniteScroll.disabled = !this.infiniteScroll.disabled;
+    }
 
-  toggleInfiniteScroll() {
-    this.infiniteScroll.disabled = !this.infiniteScroll.disabled;
-  }
-
+    getRowHeight(row) {
+        return row.height;
+    }
 
 }
